d5fc9cb600f1f21ddc3e96ec0d37e6697dc7e07c
David <dxwang2006@yahoo.com>
2015-02-04 12:37:04 -0600

    1.9 modified

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,27 +5,44 @@ public class Fibonacci {
 
 
     final static String introduction = "Fibonacci Sequence terms less than 50";
-
-
     /** Print out the Fibonacci sequence for values < 50 */
 
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
+        
+        compute();
+
+    }
+
+    public static int[] compute() {
+
         int lo = 1;
-
         int hi = 1;
+        int[] result = new int[9];
 
-
-        System.out.println(introduction);
-
-        System.out.println(lo);
-        while (hi < 50) {
-            System.out.println(hi);
+        for (int i = 0; i < 9; i++) {
+            result[i] = lo;
             hi = lo + hi;       // new hi
             lo = hi - lo;       /* new lo is (sum - old lo)
-                                  that is, the old hi */
+
+                                 that is, the old hi */
         }
+
+        System.out.println(introduction);
+        for (int i = 0; i < 9; i++) {
+
+            System.out.println(result[i]);
+        }
+
+        return result;
     }
+
+
+
+
+
+
 
 
 }
