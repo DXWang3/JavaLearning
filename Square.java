26a34b72210d3431e58be2fc233d4ef1dae2e980
David <dxwang2006@yahoo.com>
2015-02-04 11:44:01 -0600

    Exercises 1.3 and 1.4"

---
+++ Square.java
@@ -0,0 +1,18 @@
+/**
+ * Created by davidwang on 2/4/15.
+ */
+public class Square {
+
+    public static void main(String[] args) {
+        int term = 1;
+
+
+        System.out.println("Square numbers less than or equal to 2500");
+
+        while (term <= 50) {
+            System.out.println(term*term);
+            term++;
+
+        }
+    }
+}
