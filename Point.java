71b48a966c5e6535eb516d4e74520f48fc0f78e2
David <dxwang2006@yahoo.com>
2015-02-04 12:11:21 -0600

    1.8

---
+++ Point.java
@@ -0,0 +1,16 @@
+/**
+ * Created by davidwang on 2/4/15.
+ */
+public class Point {
+
+  public double x,y;
+
+   public void set(Point q){
+
+        this.x = q.x;
+        this.y = q.y;
+       
+   }
+
+
+}
