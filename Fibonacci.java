79a106f53fe13047b92cf47d8ac0fd1b330e7f55
David <dxwang2006@yahoo.com>
2015-02-04 11:51:16 -0600

    1.5 and 1.6 modifications

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,13 +3,20 @@
  */
 public class Fibonacci {
 
+
+    final static String introduction = "Fibonacci Sequence terms less than 50";
+
+
     /** Print out the Fibonacci sequence for values < 50 */
+
+
     public static void main(String[] args) {
         int lo = 1;
 
         int hi = 1;
 
-        System.out.println("Fibonacci Sequence terms less than 50");
+
+        System.out.println(introduction);
 
         System.out.println(lo);
         while (hi < 50) {
