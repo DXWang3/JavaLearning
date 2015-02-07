fd8a33b2f291676f1562c151a8a9e0d2d53ef1e5
David <dxwang2006@yahoo.com>
2015-02-06 22:01:33 -0600

    goes with ImprovedFibonacci

---
+++ FibNumber.java
@@ -0,0 +1,14 @@
+/**
+ * Created by davidwang on 2/6/15.
+ */
+public class FibNumber {
+
+    public boolean even;
+    public int number;
+
+    public FibNumber(int x, boolean y){
+
+        this.even = y;
+        this.number = x;
+    }
+}
