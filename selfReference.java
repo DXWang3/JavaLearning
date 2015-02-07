366bc85ca06937dfa45aadf1842bc13608e01c34
David <dxwang2006@yahoo.com>
2015-02-07 16:32:51 -0600

    Java code that prints itself

---
+++ selfReference.java
@@ -0,0 +1,55 @@
+/**
+ * Created by davidwang on 2/7/15.
+ */
+public class selfReference {
+
+    public static void main(String[] args){
+
+       String[] reference = {
+
+"public class selfReference {",
+
+"    public static void main(String[] args)",
+
+"       String[] reference = {",
+
+"         for(int i = 0; i < 3; i++){System.out.println(reference[i]);}",
+
+"         for(int i = 0; i < 3; i++){System.out.println((char)34 + reference[i] + (char)34 + (char)44);}",
+
+"         for(int i = 3; i < 11; i++){System.out.println((char)34 + reference[i] + (char)34) + (char)44);}",
+
+"         System.out.println(reference[10]);",
+
+"         for(int i = 3; i < 8; i++){System.out.println(reference[i]);}",
+
+"         System.out.println(reference[11]);",
+
+"         System.out.println(reference[12]);",
+
+"       };",
+
+"   }",
+
+"}",
+
+
+
+      };
+
+         for(int i = 0; i < 3; i++){System.out.println(reference[i]);}
+         for(int i = 0; i < 3; i++){System.out.println((char)34 + reference[i] + (char)34 + (char)44);}
+         for(int i = 3; i < 13; i++){System.out.println((char)34 + reference[i] + (char)34 + (char)44);}
+         System.out.println(reference[10]);
+         for(int i = 3; i < 10; i++){System.out.println(reference[i]);}
+         System.out.println(reference[11]);
+         System.out.println(reference[12]);
+
+
+
+
+
+    }
+
+
+}
