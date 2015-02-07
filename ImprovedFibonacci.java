680379120288612d96d2f90c2c1cbef457af4c50
David <dxwang2006@yahoo.com>
2015-02-06 20:15:27 -0600

    updated to version 1.14

--- ImprovedFibonacci.java
+++ ImprovedFibonacci.java
@@ -11,20 +11,61 @@ public class ImprovedFibonacci {
      * Print out the first few Fibonacci numbers,
      * marking evens with a '*'
      */
-    public static void main(String[] args) {
+    public static FibNumber[] compute() {
         int lo = 1;
         int hi = 1;
-        String mark;
+        FibNumber[] list = new FibNumber[MAX_INDEX];
 
-        System.out.println(MAX_INDEX + ": " + lo);
-        for (int i = MAX_INDEX-1; i >= 1; i--) {
-            if (hi % 2 == 0)
-                mark = " *";
-            else
-                mark = "";
-            System.out.println(i + ": " + hi + mark);
+
+        for (int i = 0; i < MAX_INDEX; i++) {
+
+
+            FibNumber first = new FibNumber (lo, false);
+
+
+            if (lo % 2 == 0){
+                first.even = true;
+            }
+
+            list[i] = first;
+
+
             hi = lo + hi;
             lo = hi - lo;
         }
+
+        return list;
     }
+
+    public static String[] storage(){
+
+        String[] deposit = new String[MAX_INDEX];
+
+        for(int i = 0; i < MAX_INDEX; i++){
+
+            String entry = compute()[i].number + ":" + compute()[i].even;
+
+            deposit[i] = entry;
+        }
+        return deposit;
+
+    }
+
+    public static void main(String[] args){
+
+        for( int i = 0; i < storage().length; i++){
+
+            System.out.println(storage()[i]);
+        }
+
+
+        for( int i = 0; i < compute().length; i++){
+
+            System.out.printf("The %sth number is %s %n", i+1, compute()[i].number);
+        }
+    }
+
+
+
+
 }
